89fd7d3ad9057818a7da836af20d4b893c148e79
Andrew Snead <andrewcsnead@gmail.com>
2014-11-13 12:09:09 +0000

    making multi sockets

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+var util = require('util');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var Game = require('./pacman');
+var Game = require('./lib/Game-server');
+var Pacman = require('./lib/pacman-server');
+
+var socket;
+var players;
 
 var game = new Game();
 
@@ -12,23 +17,55 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-io.on('connection', function(socket) {
+function init() {
+  players = [];
 
-  console.log("someone joined");
+  setEventHandlers();
 
-  socket.on('start', function() {
-    console.log("Game started");
-    game.init(io)
+  server.listen(8080, function() {
+    console.log("Server listening on port 8080");
+  });
+}
+
+var setEventHandlers = function() {
+  io.on("connection", onSocketConnection);
+};
+
+function onSocketConnection(client) {
+  util.log("New player connected" + client.id)
+
+  client.on('new:player', onNewPlayer);
+  
+  client.on('start', function() {
+      console.log("Game started");
+      game.init(io)
+    });
+
+  client.on('keypress', function(key) {
+    this.pacman.changeDirection(key);
   });
 
-  socket.on('keypress', function(key) {
-    game.keypress(key);
-  });
+  client.on("disconnect", onClientDisconnect);
+}
+
+function onClientDisconnect() {
+  util.log("Player has disconnected: " + this.id)
+}
+
+function onNewPlayer() {
+  var existingPacman;
+  var newPacman = new Pacman()
+  newPacman.id = this.id;
+  this.broadcast.emit("new pacman", {newPacman:});
+
+  for (var i = 0; i < players.length; i++) {
+    existingPacman = players[i];
+    this.emit("new pacman", {id: existingPacman})
+  }
+
+  players.push(newPacman);
+}
 
 
-});
-
-server.listen(8080, function() {
-  console.log("Server listening on port 8080");
-});
+init();
 
---
+++ public/pacman.js
@@ -0,0 +1,42 @@
+function Pacman(id) {
+  this.id = id
+}
+
+Pacman.prototype.render  = function(context) {
+  context.fillStyle = "white";
+  context.fillRect(0, 0, width, height);
+  
+  context.beginPath();
+  if (pacman.direction === 'right') {
+    context.arc(pacman.x, pacman.y, 20, 
+      (Math.PI / 180) * pacman.mouthOpenValue, 
+      (Math.PI / 180) * (360 - pacman.mouthOpenValue));
+  }
+  else if (pacman.direction === 'left'){
+    context.arc(pacman.x, pacman.y, 20,
+      (Math.PI / 180) * (179 - pacman.mouthOpenValue), //to stop flicker dont draw 180 to 180
+      (Math.PI / 180) * (180 + pacman.mouthOpenValue), true); 
+  }
+  else if (pacman.direction === 'up'){
+    context.arc(pacman.x, pacman.y, 20,
+      (Math.PI / 180) * (269 - pacman.mouthOpenValue),
+      (Math.PI / 180) * (270 + pacman.mouthOpenValue), true); 
+  }
+  else {
+    context.arc(pacman.x, pacman.y, 20,
+      (Math.PI / 180) * (89 - pacman.mouthOpenValue),
+      (Math.PI / 180) * (90 + pacman.mouthOpenValue), true); 
+  }
+
+  context.lineTo(pacman.x, pacman.y);
+  context.fillStyle = '#000';
+  context.fill();
+}
+
+Pacman.prototype.render = function() {
+  {     
+  
+    paintPacman(pacman);
+    paintSprite(sprite.x, sprite.y, sprite.size);
+    paintScore(scoreText);      
+  }   
