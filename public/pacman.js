7a89eff6c65a56d80444b33556bb05a5dc2a96ee
Andrew Snead <andrewcsnead@gmail.com>
2014-11-13 19:40:41 +0000

    mouth now working

--- public/pacman.js
+++ public/pacman.js
@@ -5,31 +5,29 @@ function Pacman(id) {
   this.radius = 10;
   this.clearance = this.radius * 2;
   this.speed = 5;
-  this.mouthOpenValue = 40;
-  this.mouthPosition = -1;
 }
 
 Pacman.prototype.render = function(context, status) {
   context.beginPath();
   if (status.direction === 'right') {
     context.arc(status.x, status.y, 20, 
-      (Math.PI / 180) * this.mouthOpenValue, 
-      (Math.PI / 180) * (360 - this.mouthOpenValue));
+      (Math.PI / 180) * status.mouthOpenValue, 
+      (Math.PI / 180) * (360 - status.mouthOpenValue));
   }
   else if (status.direction === 'left'){
     context.arc(status.x, status.y, 20,
-      (Math.PI / 180) * (179 - this.mouthOpenValue), //to stop flicker dont draw 180 to 180
-      (Math.PI / 180) * (180 + this.mouthOpenValue), true); 
+      (Math.PI / 180) * (179 - status.mouthOpenValue), //to stop flicker dont draw 180 to 180
+      (Math.PI / 180) * (180 + status.mouthOpenValue), true); 
   }
   else if (status.direction === 'up'){
     context.arc(status.x, status.y, 20,
-      (Math.PI / 180) * (269 - this.mouthOpenValue),
-      (Math.PI / 180) * (270 + this.mouthOpenValue), true); 
+      (Math.PI / 180) * (269 - status.mouthOpenValue),
+      (Math.PI / 180) * (270 + status.mouthOpenValue), true); 
   }
   else {
     context.arc(status.x, status.y, 20,
-      (Math.PI / 180) * (89 - this.mouthOpenValue),
-      (Math.PI / 180) * (90 + this.mouthOpenValue), true); 
+      (Math.PI / 180) * (89 - status.mouthOpenValue),
+      (Math.PI / 180) * (90 + status.mouthOpenValue), true); 
   }
 
   context.lineTo(status.x, status.y);
